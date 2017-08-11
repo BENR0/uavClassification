4f0cacead6510b9326952712f3291e80604a0db6
Ro <Ro@nazuna-2.local>
2017-08-11 19:08:47 +0200

    Change small changes

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from uavClassification import *
 
 inputImg = "../orthofoto_small_cubic_resample.tif"
 
-#MeanShiftSmoothing(inputImg, "../smooth_out.tif", "../smooth_position.tif")
+MeanShiftSmoothing(inputImg, "../smooth_out.tif", "../smooth_position.tif")
 
 #print("segmentation")
 #MeanShiftSegmentation("../smooth_out.tif", "../segmented.tif")
@@ -13,11 +13,11 @@ inputImg = "../orthofoto_small_cubic_resample.tif"
 
 
 print("numpy")
-smoothed = MeanShiftSmoothing(inputImg, "test.tif", "test.tif", outnp = True)
+smoothed = MeanShiftSmoothing(inputImg, None, None, outnp = True)
 
 print("segmentation")
 
-segmented = MeanShiftSegmentation(smoothed, "test.tif", innp = True, outnp = True)
+segmented = MeanShiftSegmentation(smoothed, None,  innp = True, outnp = True)
 
 print("colormapping")
 colorMapping(segmented, "../labeled_np.tif", innp = True)
--- uavClassification/classification.py
+++ uavClassification/classification.py
@@ -23,6 +23,9 @@ import otbApplication
 logger = logging.getLogger(__name__)
 
 
+#numpy get vector as array
+#.GetVectorImageAsNumpyArray("out", "float")
+
 ######################
 # MEAN SHIFT FILTERING
 ######################
@@ -70,7 +73,7 @@ def MeanShiftSmoothing(inData, outSmooth, outPosition, spatialr = 16, ranger = 1
 
     if outnp:
 	app.Execute()
-	appOutput = app.GetImageAsNumpyArray("out")
+	appOutput = app.GetImageAsNumpyArray("fout")
 	appPosition = app.GetImageAsNumpyArray("foutpos")
         return appOutput
     else:
@@ -192,5 +195,30 @@ def colorMapping(inData, outData, method = "optimal", innp = False, outnp = Fals
 	app.SetParameterString("out", outData)
 	app.ExecuteAndWriteOutput()
 
+
+ # def segmentStats(inData):
+    # """Calculate statistical moments for every segment
+
+    # Parameters
+    # ----------
+    # inData: numpy array
+        # Array with segments
+
+
+    # Return
+    # ------
+
+    # """
+    # segmentIds = np.unique(inData)
+
+
+
+    # return
+
+
+# def classify():
+
+
+    # return
 
 
